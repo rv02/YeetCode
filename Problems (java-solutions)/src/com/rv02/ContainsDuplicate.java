b3b7043bcd67cde950399de4d91eb538521b2fe0
Rishabh Verma <rv02012001@gmail.com>
2021-08-16 01:14:31 +0530

    solved Contains Duplicate

---
+++ Problems (java-solutions)/src/com/rv02/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package com.rv02;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num: nums) {
+            if (numSet.contains(num)) {
+                return true;
+            }
+            numSet.add(num);
+        }
+        return false;
+    }
+}
