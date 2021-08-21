6352c7590c8edb15f51eecf754c73d97784dbfb1
Rishabh Verma <rv02012001@gmail.com>
2021-08-21 21:07:37 +0530

    solved Minimum in rotated array

---
+++ Problems (java-solutions)/src/com/rv02/MinInRotatedArray.java
@@ -0,0 +1,19 @@
+package com.rv02;
+
+public class MinInRotatedArray {
+    public int findMin(int[] nums) {
+        int high = nums.length - 1, low = 0;
+        while (low < high) {
+            if (nums[low] < nums[high]) {
+                return nums[low];
+            }
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[low]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
